ac6d3955f760d125396dc27ebff6b8aec71acedb
Yudin Dennis <yudin@1mps.ru>
2023-08-30 16:34:48 +0300

    impl feature get all groups with less or equals amount of students

--- src/main/java/dev/yudin/dao/GroupDAO.java
+++ src/main/java/dev/yudin/dao/GroupDAO.java
@@ -5,8 +5,7 @@ import dev.yudin.entities.Group;
 import java.util.List;
 
 public interface GroupDAO {
-
 	List<Group> findAll();
-
 	void save(List<String> groups);
+	List<String> findAll(int amountStudents);
 }
